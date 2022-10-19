986c2006360c4677c2e5dcb46c186161c5bfb1e3
Henry Augusta <henryaugusta4@gmail.com>
2022-10-19 08:03:26 +0700

    add cocktail Api and datasource

---
+++ core/src/main/java/com/feylabs/core/data/remote/CocktailDataSource.kt
@@ -0,0 +1,66 @@
+package com.feylabs.core.data.remote
+
+import com.feylabs.core.domain.*
+import com.feylabs.core.domain.DrinkAlcoholicCategory.DrinkAlcoholicCategoryItem
+import com.feylabs.core.domain.DrinkGlassCategory.DrinkGlassCategoryItem
+import com.feylabs.core.domain.DrinkIngredientCategory.DrinkIngredientCategoryItem
+import com.feylabs.core.network.service.CocktailApi
+import com.feylabs.core.network.service.JavalovaApi
+import com.feylabs.core.util.AppResult
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.Flow
+
+class CocktailDataSource(
+    private val api: CocktailApi,
+) {
+
+    suspend fun getGeneralCategory(): Flow<AppResult<List<DrinkGeneralCategory.DrinkGeneralCategoryItem?>>> {
+        return flow {
+            emit(AppResult.Loading())
+            val res = api.getGeneralCategory()
+            if (res.isSuccessful){
+                emit(AppResult.Success(res.body()))
+            }else{
+                emit(AppResult.Error(res.errorBody().toString()))
+            }
+        }
+    }
+
+    suspend fun getGlassCategory(): Flow<AppResult<List<DrinkGlassCategoryItem?>>> {
+        return flow {
+            emit(AppResult.Loading())
+            val res = api.getGlassCategory()
+            if (res.isSuccessful){
+                emit(AppResult.Success(res.body()))
+            }else{
+                emit(AppResult.Error(res.errorBody().toString()))
+            }
+        }
+    }
+
+    suspend fun getIngredientCategory(): Flow<AppResult<List<DrinkIngredientCategoryItem?>>> {
+        return flow {
+            emit(AppResult.Loading())
+            val res = api.getIngredientCategory()
+            if (res.isSuccessful){
+                emit(AppResult.Success(res.body()))
+            }else{
+                emit(AppResult.Error(res.errorBody().toString()))
+            }
+        }
+    }
+
+    suspend fun getAlcoholicCategory(): Flow<AppResult<List<DrinkAlcoholicCategoryItem?>>> {
+        return flow {
+            emit(AppResult.Loading())
+            val res = api.getAlcoholicCategory()
+            if (res.isSuccessful){
+                emit(AppResult.Success(res.body()))
+            }else{
+                emit(AppResult.Error(res.errorBody().toString()))
+            }
+        }
+    }
+
+}
+
--- core/src/main/java/com/feylabs/core/network/service/CocktailApi.kt
+++ core/src/main/java/com/feylabs/core/network/service/CocktailApi.kt
@@ -1,11 +1,26 @@
 package com.feylabs.core.network.service
 
-import com.feylabs.core.domain.QuoteApiResponse
+import com.feylabs.core.domain.*
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface CocktailApi {
 
-    @GET("quotes")
-    suspend fun getAllJavalova(): Response<List<QuoteApiResponse.QuoteApiResponseItem>>
+    @GET("list.php?c=list")
+    suspend fun getGeneralCategory(
+    ): Response<List<DrinkGeneralCategory.DrinkGeneralCategoryItem>>
+
+    @GET("list.php?g=list")
+    suspend fun getGlassCategory(
+    ): Response<List<DrinkGlassCategory.DrinkGlassCategoryItem>>
+
+    @GET("list.php?i=list")
+    suspend fun getIngredientCategory(
+    ): Response<List<DrinkIngredientCategory.DrinkIngredientCategoryItem>>
+
+    @GET("list.php?a=list")
+    suspend fun getAlcoholicCategory(
+    ): Response<List<DrinkAlcoholicCategory.DrinkAlcoholicCategoryItem>>
+
 }
